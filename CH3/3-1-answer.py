1d7145075b99ebf8d4eafb77a63c1698f6be3b88
SeoMiYoung <seo20seo00@naver.com>
2024-07-12 19:07:42 +0900

    3-1변경 - 이름을 잘 짓자

--- CH3/3-1-answer.py
+++ CH3/3-1-answer.py
@@ -2,9 +2,9 @@ n = 1260
 count = 0
 
 # 큰 단위의 화폐부터 차례대로 확인
-list = [500, 100, 50, 10]
+money_list = [500, 100, 50, 10]
 
-for coin in list:
+for coin in money_list:
     count += n // coin
     n %= coin
     
